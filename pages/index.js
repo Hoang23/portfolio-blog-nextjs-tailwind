44d955d2da2f5c4a5445bd024aaadc3b25e6b1d2
hoang23 <3hoang96@gmail.com>
2022-09-01 18:32:48 +1000

    update home page colors and text

--- pages/index.js
+++ pages/index.js
@@ -23,33 +23,40 @@ export default function Home({ posts }) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Hi I'm Hoang
+            Hello, <br></br>
+            I'm{' '}
+            <span className="text-grey-900 dark:text-grey-900 text-3xl font-bold lg:text-5xl">
+              Hoang
+            </span>
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-100">Software Engineer</p>
+          <p className="lg:text-2x my-2 text-lg lg:my-4">Software Engineer</p>
 
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-300">
-            Read more &nbsp;
+          {/* font-light lg:text-xl */}
+          <p className="text-lg font-light leading-7 text-gray-500 dark:text-gray-300">
+            Read more
             <Link className="ml-2 mr-2 font-normal text-black" href="/about">
               <RoughNotation
                 show
                 type="highlight"
                 animationDelay={250}
                 animationDuration={2000}
-                color={'pink'}
+                color={'#CCFBF1'}
               >
                 about me
               </RoughNotation>
             </Link>
-            &nbsp; or &nbsp;
-            <RoughNotation
-              show
-              type="highlight"
-              animationDelay={250}
-              animationDuration={2000}
-              color={'violet'}
-            >
-              my resume
-            </RoughNotation>
+            or see my
+            <Link className="ml-2 mr-2 font-normal text-black" href="/projects">
+              <RoughNotation
+                show
+                type="highlight"
+                animationDelay={250}
+                animationDuration={2000}
+                color={'#CCFBF1'}
+              >
+                projects
+              </RoughNotation>
+            </Link>
           </p>
         </div>
       </div>
